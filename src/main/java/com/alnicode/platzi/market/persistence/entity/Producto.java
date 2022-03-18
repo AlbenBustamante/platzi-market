dd8441bd874171774b3e05a934cf7d391954b03c
alnicode32 <albensnico@gmail.com>
2022-03-18 11:37:44 -0500

    Establecida la relación entre categorías y productos

--- src/main/java/com/alnicode/platzi/market/persistence/entity/Producto.java
+++ src/main/java/com/alnicode/platzi/market/persistence/entity/Producto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Getter
@@ -35,4 +37,8 @@ public class Producto {
     private Integer cantidadStock;
 
     private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
+    private Categoria categoria;
 }
