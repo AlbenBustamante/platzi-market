9461bdc2eeeee90b214c288ca6042f3babc4ecf4
alnicode32 <albensnico@gmail.com>
2022-03-17 21:48:24 -0500

    Inicio del proyecto

---
+++ src/main/java/com/alnicode/platzi/market/PlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.alnicode.platzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
