942dfa67bac65c351a933ccc2c80f701adb2755a
alnicode32 <albensnico@gmail.com>
2022-03-17 22:25:11 -0500

    Creada la entidad Compra

---
+++ src/main/java/com/alnicode/platzi/market/persistence/entity/Compra.java
@@ -0,0 +1,35 @@
+package com.alnicode.platzi.market.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "compras")
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private LocalDate fecha;
+
+    @Column(table = "medio_pago")
+    private Character medioPago;
+
+    private String comentario;
+
+    private Character estado;
+}
