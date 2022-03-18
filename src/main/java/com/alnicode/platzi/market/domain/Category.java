bc726e87d49ad35d08daee08164e264e0d06da67
alnicode32 <albensnico@gmail.com>
2022-03-18 14:47:08 -0500

    Creados los modelos que serán mapeados de las entidades

---
+++ src/main/java/com/alnicode/platzi/market/domain/Category.java
@@ -0,0 +1,12 @@
+package com.alnicode.platzi.market.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+    private int categoryId;
+    private String description;
+    private boolean active;
+}
