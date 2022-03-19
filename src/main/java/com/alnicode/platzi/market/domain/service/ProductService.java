446d039a800e74a7a38551d97c1cc0b69ad02450
alnicode32 <albensnico@gmail.com>
2022-03-18 19:38:24 -0500

    Creados los métodos para toda la lógica del servicio de productos

--- src/main/java/com/alnicode/platzi/market/domain/service/ProductService.java
+++ src/main/java/com/alnicode/platzi/market/domain/service/ProductService.java
@@ -1,12 +1,40 @@
 package com.alnicode.platzi.market.domain.service;
 
+import com.alnicode.platzi.market.domain.Product;
 import com.alnicode.platzi.market.domain.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    public List<Product> getAll() {
+        return this.productRepository.getAll();
+    }
+
+    public List<Product> getByCategory(int categoryId) {
+        return this.productRepository.getByCategory(categoryId);
+    }
+
+    public Optional<Product> getProduct(int productId) {
+        return this.productRepository.getProduct(productId);
+    }
+
+    public Product save(Product product) {
+        return this.productRepository.save(product);
+    }
+
+    public boolean delete(int productId) {
+        try {
+            this.productRepository.delete(productId);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
 }
