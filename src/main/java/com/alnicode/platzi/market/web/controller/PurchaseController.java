cdb82d93be39b6c000ed290bd22041f2a3c5894a
alnicode32 <albensnico@gmail.com>
2022-03-19 18:09:43 -0500

    Creado método para guardar una compra en el controlador

--- src/main/java/com/alnicode/platzi/market/web/controller/PurchaseController.java
+++ src/main/java/com/alnicode/platzi/market/web/controller/PurchaseController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,10 @@ public class PurchaseController {
     @GetMapping("/clients/{id}")
     public ResponseEntity<List<Purchase>> getByClient(@PathVariable("id") String clientId) {
         return ResponseEntity.of(this.purchaseService.getByClient(clientId));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
+        return new ResponseEntity<>(this.purchaseService.save(purchase), HttpStatus.CREATED);
     }
 }
