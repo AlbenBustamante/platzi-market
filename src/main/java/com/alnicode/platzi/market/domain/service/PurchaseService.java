77d95816bf4c99a0c89050bc1d98030add393331
alnicode32 <albensnico@gmail.com>
2022-03-19 18:03:41 -0500

    Creado servicio para las compras

---
+++ src/main/java/com/alnicode/platzi/market/domain/service/PurchaseService.java
@@ -0,0 +1,27 @@
+package com.alnicode.platzi.market.domain.service;
+
+import com.alnicode.platzi.market.domain.Purchase;
+import com.alnicode.platzi.market.persistence.CompraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+    @Autowired
+    private CompraRepository compraRepository;
+
+    List<Purchase> getAll() {
+        return this.compraRepository.getAll();
+    }
+
+    Optional<List<Purchase>> getByClient(String clientId) {
+        return this.compraRepository.getByClient(clientId);
+    }
+
+    Purchase save(Purchase purchase) {
+        return this.compraRepository.save(purchase);
+    }
+}
