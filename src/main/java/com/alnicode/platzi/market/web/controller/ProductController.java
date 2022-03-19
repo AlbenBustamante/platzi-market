15496e9c33d47288d63032e9a2e79dc45292b76a
alnicode32 <albensnico@gmail.com>
2022-03-18 19:54:28 -0500

    Creada la base para el controlador de los productos junto a la inyección de la dependencia del servicio

---
+++ src/main/java/com/alnicode/platzi/market/web/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.alnicode.platzi.market.web.controller;
+
+import com.alnicode.platzi.market.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+}
