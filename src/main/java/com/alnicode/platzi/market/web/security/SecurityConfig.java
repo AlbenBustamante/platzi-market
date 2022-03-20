72f01c4e525af7c1b5f927192f2fe80ddafd7590
alnicode32 <albensnico@gmail.com>
2022-03-20 13:35:58 -0500

    Creado método para que todas las peticiones que terminen en 'authenticate', las permita

--- src/main/java/com/alnicode/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/alnicode/platzi/market/web/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.alnicode.platzi.market.web.security;
 import com.alnicode.platzi.market.domain.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,5 +15,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(this.userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated();
     }
 }
