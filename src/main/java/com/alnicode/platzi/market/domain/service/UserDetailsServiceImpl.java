80d2678df34a6a3b07c0864f0efa3b044252e9b1
alnicode32 <albensnico@gmail.com>
2022-03-19 20:13:12 -0500

    Creada implementación de UserDetailsService de Security

---
+++ src/main/java/com/alnicode/platzi/market/domain/service/UserDetailsServiceImpl.java
@@ -0,0 +1,12 @@
+package com.alnicode.platzi.market.domain.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
