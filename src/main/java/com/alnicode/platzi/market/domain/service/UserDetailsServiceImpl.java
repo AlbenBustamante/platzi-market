4fcd17a20cf8c0a2f32fb1b60aa2799b47e7a309
alnicode32 <albensnico@gmail.com>
2022-03-19 20:23:42 -0500

    Agregado los parámetros para el usuario (hardcode)

--- src/main/java/com/alnicode/platzi/market/domain/service/UserDetailsServiceImpl.java
+++ src/main/java/com/alnicode/platzi/market/domain/service/UserDetailsServiceImpl.java
@@ -5,9 +5,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.ArrayList;
+
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User();
+        return new User("username", "{noop}password", new ArrayList<>());
     }
 }
