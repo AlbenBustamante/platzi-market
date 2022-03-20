57688bffae653cf53fafe07bcf130a55de5076a1
alnicode32 <albensnico@gmail.com>
2022-03-20 13:37:40 -0500

    Definido el gestor de la autenticación de la aplicación

--- src/main/java/com/alnicode/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/alnicode/platzi/market/web/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.alnicode.platzi.market.web.security;
 
 import com.alnicode.platzi.market.domain.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,5 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
