eec4d8cafc08c8936055fbba7a8237df2cc44fd6
alnicode32 <albensnico@gmail.com>
2022-03-20 18:30:57 -0500

    Finalizado el módulo de seguridad

--- src/main/java/com/alnicode/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/alnicode/platzi/market/web/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.alnicode.platzi.market.web.security;
 
 import com.alnicode.platzi.market.domain.service.UserDetailsServiceImpl;
+import com.alnicode.platzi.market.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,11 +10,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,7 +29,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(this.jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
