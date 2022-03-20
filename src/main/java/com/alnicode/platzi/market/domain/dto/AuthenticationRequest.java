a72e94db6910098358f981b5c475b4bcaebe557d
alnicode32 <albensnico@gmail.com>
2022-03-20 12:56:56 -0500

    Creadas las clases para el manejo de los jwt

---
+++ src/main/java/com/alnicode/platzi/market/domain/dto/AuthenticationRequest.java
@@ -0,0 +1,11 @@
+package com.alnicode.platzi.market.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/alnicode/platzi/market/domain/dto/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.alnicode.platzi.market.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class AuthenticationResponse {
+    private String jwt;
+}
