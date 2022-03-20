65b4c4be07fc0e6d2b7d12ebac7335e6fbcaae53
alnicode32 <albensnico@gmail.com>
2022-03-19 20:39:37 -0500

    Agregada la anotación de Servicio y configuración de SecurityConfig

---
+++ src/main/java/com/alnicode/platzi/market/web/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.alnicode.platzi.market.web.security;
+
+import com.alnicode.platzi.market.domain.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService);
+    }
+}
